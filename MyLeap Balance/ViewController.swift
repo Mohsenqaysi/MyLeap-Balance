7dd22056941a07af849763d77033fa27465b1aa2
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2018-10-06 19:07:00 +0100

    Initial Commit

---
+++ MyLeap Balance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLeap Balance
+//
+//  Created by Mohsen Qaysi on 10/6/18.
+//  Copyright © 2018 Mohsen Qaysi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
