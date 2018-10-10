54fa47295201bb5902d8c630519160ef6577cc5b
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2018-10-10 14:24:41 +0100

    Login view works

--- MyLeap Balance/Controllers/ViewController.swift
+++ MyLeap Balance/Controllers/ViewController.swift
@@ -9,12 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var cardInfo: CardInfo?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(cardInfo)
+
     }
-
-
 }
-
